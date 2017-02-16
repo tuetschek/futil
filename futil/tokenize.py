54f64872d505fec9abdd422c53ec01bfe4873e30
Ondrej Dusek <o.dusek@hw.ac.uk>
2017-02-16 15:52:01 +0000

    Treex-like English tokenization

---
+++ futil/tokenize.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -"- coding: utf-8 -"-
+
+from __future__ import unicode_literals
+
+def tokenize(text):
+    """Tokenize text (separate tokens by spaces)"""
+
+    toks = text
+    # separate quotes everywhere
+    toks = re.sub(r'(["<>{}“”«»–|—„‚‘]|\[|\]|``|\'\'|‘‘|\^)', r' \1 ', toks)
+
+    # the following characters (double-characters) are separated everywhere (except inside URLs)
+    toks = re.sub(r'([;!()?#\$£%&*…]|--)', r' \1 ', toks)
+
+    # short hyphen is separated if it is followed or preceeded by non-alphanuneric character and
+    # is not a part of --, or a unary minus
+    toks = re.sub(r'([^\-\w])\-([^\-0-9])', r'\1 - \2', toks)
+    toks = re.sub(r'([0-9]\s+)\-([0-9])', r'\1 - \2', toks)  # preceded by a number - not a unary minus
+    toks = re.sub(r'([^\-])\-([^\-\w])', r'\1 - \2', toks)
+
+    # plus is separated everywhere, except at the end of a word (separated by a space) and as unary plus
+    toks = re.sub(r'(\w)\+(\w)', r'\1 + \2', toks)
+    toks = re.sub(r'([0-9]\s*)\+([0-9])', r'\1 + \2', toks)
+    toks = re.sub(r'\+([^\w\+])', r'+ \1', toks)
+
+    # apostrophe is separated if it is followed or preceeded by non-alphanumeric character,
+    # is not part of '', and is not followed by a digit (e.g. '60).
+    toks = re.sub(r'([^\'’\w])([\'’])([^\'’\d])', r'\1 \2 \3', toks)
+    toks = re.sub(r'([^\'’])([\'’])([^\'’\w])', r'\1 \2 \3', toks)
+
+    # dot, comma, slash, and colon are separated if they do not connect two numbers
+    toks = re.sub(r'(\D|^)([\.,:\/])', r'\1 \2', toks)
+    toks = re.sub(r'([\.,:\/])(\D|$)', r'\1 \2', toks)
+
+    # three dots belong together
+    toks = re.sub(r'\.\s*\.\s*\.', r'...', toks)
+
+    # most common contractions
+    toks = re.sub(r'([\'’´])(s|m|d|ll|re|ve)\s', r' \1\2 ', toks)  # I'm, I've etc.
+    toks = re.sub(r'(n[\'’´]t\s)', r' \1 ', toks)  # do n't
+
+    # other contractions, as implemented in Treex
+    toks = re.sub(r' ([Cc])annot\s', r' \1an not ', toks)
+    toks = re.sub(r' ([Dd])\'ye\s', r' \1\' ye ', toks)
+    toks = re.sub(r' ([Gg])imme\s', r' \1im me ', toks)
+    toks = re.sub(r' ([Gg])onna\s', r' \1on na ', toks)
+    toks = re.sub(r' ([Gg])otta\s', r' \1ot ta ', toks)
+    toks = re.sub(r' ([Ll])emme\s', r' \1em me ', toks)
+    toks = re.sub(r' ([Mm])ore\'n\s', r' \1ore \'n ', toks)
+    toks = re.sub(r' \'([Tt])is\s', r' \'\1 is ', toks)
+    toks = re.sub(r' \'([Tt])was\s', r' \'\1 was ', toks)
+    toks = re.sub(r' ([Ww])anna\s', r' \1an na ', toks)
+
+    # clean extra space
+    toks = re.sub(r'\s+', ' ', toks)
+    toks = toks.strip()
+    return toks
+
+
