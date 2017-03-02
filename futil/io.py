ccb1e089858a46960432753f47e1e42d418c4a0f
Ondrej Dusek <o.dusek@hw.ac.uk>
2017-03-02 15:42:19 +0000

    Reading lines

--- futil/io.py
+++ futil/io.py
@@ -7,6 +7,15 @@ from __future__ import print_function
 import codecs
 
 
+def read_lines(file_name):
+    """Write lines to a file."""
+    buf = []
+    with codecs.open(file_name, 'rb', 'UTF-8') as fh:
+        for line in fh:
+            buf.append(line)
+    return buf
+
+
 def write_lines(file_name, lines):
     """Write lines to a file."""
     with codecs.open(file_name, 'wb', 'UTF-8') as fh:
