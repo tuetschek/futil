b2742ad14be2514962bc91f038b04fc8a6575c56
Ondrej Dusek <o.dusek@hw.ac.uk>
2017-04-05 15:35:53 +0100

    Option to only output dialogues of specified length

--- futil/dialogue.py
+++ futil/dialogue.py
@@ -4,13 +4,15 @@
 from __future__ import unicode_literals
 
 
-def create_turns(dialogues, history_length):
+def create_turns(dialogues, history_length, exact_len_only=False):
     """Given a list of dialogues (list of lists), create context-response turns, with the given
     history length in contexts (0 = just the preceding utterance)."""
 
     data = []
     for dialogue in dialogues:
         for pos, resp in enumerate(dialogue[1:], start=1):
+            if exact_len_only and pos != history_length:
+                continue
             context = ' <B> '.join(dialogue[max(0, pos - history_length - 1):pos])
             data.append((context, resp))
     return data
