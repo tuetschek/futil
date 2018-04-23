c58222d747fa20256d57c20b1b2537078659afb4
Ondrej Dusek <o.dusek@hw.ac.uk>
2018-04-23 17:06:16 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+        name='futil',
+        version='0.0.1',
+        description='Common helper file functions',
+        author='Ondrej Dusek',
+        author_email='tuetschek@gmail.com',
+        url='https://github.com/tuetschek/futil',
+        download_url='https://github.com/tuetschek/futil.git',
+        license='Apache 2.0',
+        packages=find_packages(),
+)
